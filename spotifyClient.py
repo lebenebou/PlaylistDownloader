29f2dcdf3802853ecef6b0602360b40de4e4969d
Youssef Yammine <lebenbou@gmail.com>
2023-05-24 10:41:28 +0300

    added exception handling for invalid link and noInternet

--- spotifyClient.py
+++ spotifyClient.py
@@ -1,18 +1,29 @@
 
+import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def getPlaylistJSON(playlistLink: str) -> dict:
+clientID = "38caf74efb5e4e5ba39bf377069c6201"
+clientSecret = "268cbdbfeb2a47d5bad2daf65a7880c0"
 
-    clientID = "38caf74efb5e4e5ba39bf377069c6201"
-    clientSecret = "268cbdbfeb2a47d5bad2daf65a7880c0"
-    
-    client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+client_credentials_manager = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def getPlaylistJSON(playlistLink: str) -> dict:
     
     playlist_id = playlistLink.split('/')[-1]  # Extract playlist ID from the link
     
-    playlist = sp.playlist(playlist_id)
+    try:
+        playlist = sp.playlist(playlist_id)
+
+    except spotipy.exceptions.SpotifyException:
+
+        return {"error":"invalid_link"}
+    
+    except requests.ConnectionError:
+
+        return {"error":"no_internet"}
+
     return playlist
 
 def getTrackNames(playlistJSON: dict) -> list[tuple[str]]:
@@ -33,6 +44,12 @@ if __name__=="__main__":
     link = str(input("Playlist Link: "))
 
     playlist = getPlaylistJSON(link)
+
+    if len(playlist)==1:
+
+        print(playlist["error"])
+        exit(0)
+
     tracks = getTrackNames(playlist)
 
     for track in tracks:
